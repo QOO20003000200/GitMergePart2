e8b22d421800d62b28c168d84147233593762b0c
Naomi Plasterer <nplaster@mymail.mines.edu>
2013-02-22 15:16:40 -0700

    addjavacode
    Jave code added to repo

---
+++ TheMotivator.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+
+public class TheMotivator {
+	private ArrayList<String> events = new ArrayList<String>();
+	
+	public void thoughtfortheday(int score) {
+		if (score == 100)
+			System.out.println("You're awesome");
+		else if (score > 90)
+			System.out.println("That's great");
+		else if (score > 60)
+			System.out.println("That's good ");
+		else
+			System.out.println("On the other hand you have different fingers");
+	}
+	
+	public void upcomingEvents(){
+		System.out.println("Upcoming Events - Please join us!");
+		for (String event : events)
+			System.out.println(event);
+	}
+	
+	public void createEvents() {
+		events.add("We're going to a movie on Saturday");
+		events.add("Study session on Sunday - Jim's house");
+	}
+	
+	public static void main(String[] args) {
+		TheMotivator tm = new TheMotivator();
+		tm.thoughtfortheday(60);
+		tm.createEvents();
+	}
+}
